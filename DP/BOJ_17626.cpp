572f7234238c61d146a85bb3801e44530fad24f3
chungchae <chungchae0@gmail.com>
2025-09-05 13:42:05 +0900

    기존 파일 이동

---
+++ DP/BOJ_17626.cpp
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <numeric>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <map>
+#include <set>
+
+using namespace std;
+
+// BOJ 17626 Four Squares (DP) (S3)
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int N; cin >> N;
+	
+	// 초기화
+	int DP[50001]; DP[0] = 0;
+
+	for (int i = 1; i <= N; i++) {
+		// 최악의 경우 1^2 + 1^2 + ... (i번)
+		DP[i] = i;
+		// 제곱수가 가능한 경우 찾기
+		for (int j = 1; j * j <= i; j++) {
+			DP[i] = min(DP[i], DP[i - j * j] + 1);
+		}
+	}
+	cout << DP[N];
+}
+
+
---
+++ Math/BOJ_1629.cpp
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <numeric>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <limits.h>
+
+using namespace std;
+
+// BOJ 1629 곱셉 (math) (S1)
+
+long long halfPow(long long A, long long B, long long C) {
+    if (B == 0)return 1;
+    long long half = halfPow(A, B / 2, C);
+    long long result = (half * half) % C;
+    if (B % 2 == 1) {
+        result = (result * A) % C;
+    }
+    return result;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long A, B, C; cin >> A >> B >> C;
+
+    cout << halfPow(A, B, C);
+}
