9ca1ec64eee6eb240f532862338956f25d65c78b
chungchae <chungchae0@gmail.com>
2025-11-27 22:33:21 +0900

    PGS/*/PCCP모의1-1

---
+++ Implementation/PGS_ 모의1-1.cpp
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <numeric>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <limits.h>
+
+using namespace std;
+
+// PGS PCCP 모의1-1 (Implementation) (*)
+string solution(string input_string) {
+    string answer = "";
+
+    int n = input_string.length();
+    map<char, int> m;
+
+    for (int i = 0; i < n; i++) {
+        char c = input_string[i];
+        while (input_string[i] == c)i++;
+        i--;
+        m[c]++;
+    }
+    for (auto a : m) {
+        if (a.second > 1) {
+            answer += a.first;
+        }
+    }
+    if (answer == "") return "N";
+    return answer;
+}
